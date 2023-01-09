c6ac007d1994cc078d4a670de195da241b490fc9
tian <tianwu@outlook.com>
2023-01-10 08:09:47 +1100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -21,11 +21,8 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{" "}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
+        <p>Building cool sh**</p>
+        <p>Best before day: 09/16/2023</p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
